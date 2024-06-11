7f5b6e5f0438c870528471f2c83c11206f33b9eb
IJLAL <ijlal.tanveer293@gmail.com>
2024-06-11 13:08:44 +0500

    another day completed

--- 14 backend/3.4 Middleware/index4.js
+++ 14 backend/3.4 Middleware/index4.js
@@ -4,4 +4,22 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 
 
-const ap
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile( __dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  var bandName = req.body["street"] + req.body["pet"]
+  res.send("<h1>this is me </h1> <h2>" + bandName + req.body.pet + "</h2>");
+  console.log(req.body);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
