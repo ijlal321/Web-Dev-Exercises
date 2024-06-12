39f3bfc8db6ee9ee54f7cb845d3c8124a3dbce8e
IJLAL <ijlal.tanveer293@gmail.com>
2024-06-12 11:53:13 +0500

    more added

--- 14 backend/4.2 Passing Data/index.js
+++ 14 backend/4.2 Passing Data/index.js
@@ -1,14 +1,30 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  const data = {
+    upperText: "<h1>Enter Your Name Below</h1>",
+  };
+  res.render("index.ejs", data);
+});
 
-app.post("/submit", (req, res) => {});
+
+app.post("/submit", (req, res) => {
+  var nrWords = req.body["fName"].length + req.body["lName"].length;
+  const data = {
+    upperText: "<h1>Your Answer Contains "+ nrWords + " words </h1>",
+  };
+  res.render("index.ejs", data);
+});
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
