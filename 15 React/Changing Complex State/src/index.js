990c324f3a676da33b001e8bd3870c4415d07ced
IJLAL <ijlal.tanveer294@gmail.com>
2024-06-26 12:50:56 +0500

    new exercises added

---
+++ 15 React/Changing Complex State/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//CHALLENGE: Make the code in App.jsx work.
+//The final app should have a single contact
+//with fName, lName and email.
